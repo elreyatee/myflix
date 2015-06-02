8dbcdfc1e2d6cece90a8bcfd33d8a235444425f7
Ellery Temple Jr <elreyatee@gmail.com>
2015-06-02 06:30:13 -0700

    fixed bracket spacing in spec files to keep with Ruby styling

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,4 +3,8 @@ class Video < ActiveRecord::Base
   belongs_to :category
   scope :sorted, ->{order(:title)}
   validates_presence_of :title, :description
+
+  def self.search_by_title(title)
+    
+  end
 end
