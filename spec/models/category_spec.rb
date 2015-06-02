2fadb2e4daa330b5c50d8ce978c14912e49fd788
Ellery Temple Jr <elreyatee@gmail.com>
2015-06-01 22:31:44 -0700

    added shoulda_matters

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,20 +1,5 @@
 require 'spec_helper'
 
 describe Category do 
-  it "saves itself" do
-    category = Category.new(name: "Action")
-    category.save
-    expect(Category.first).to eq(category)
-  end
-
-  it "has many videos" do
-    comedies = Category.create(name: "Comedies")
-    south_park = Video.create(title: "South Park", description: "Funny video!", category: comedies)
-    futurama = Video.create(title: "Futurama", description: "Space travel!", category: comedies)
-    expect(comedies.videos).to include(futurama, south_park)
-  end
-
-  it "does not save without a name" do
-    category = Category.create
-    expect(Category.count).to eq(0)
-  end
+  it {should have_many(:videos)}
+end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,24 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: "Superman", description: "Man of Steel")
-    video.save
-    expect(Video.first).to eq(video)
-  end
-
-  it "belongs to category" do
-    dramas = Category.create(name: "dramas")
-    monk = Video.create(title: "Monk", description: "A drama", category: dramas)
-    expect(monk.category).to eq(dramas)
-  end
-
-  it "does not save a video without a title" do
-    video = Video.create(description: "A good video!")
-    expect(Video.count).to eq(0)
-  end
-
-  it "does not save a video without a description" do
-    video = Video.create(title: "Monk")
-    expect(Video.count).to eq(0)
-  end
+  it { should belong_to(:category)}
+  it { should validate_presence_of(:title)}
+  it { should validate_presence_of(:description)}
+end
