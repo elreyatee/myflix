2b27383e7a4c75c73058275b271c84c90bd198d1
Ellery Temple Jr <elreyatee@gmail.com>
2015-06-03 07:47:08 -0700

    added search term in UI

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,4 +6,8 @@ class VideosController < ApplicationController
   def show 
     @video = Video.find_by(id: params[:id])
   end
+
+  def search
+    @results = Video.search_by_title(params[:search_term])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
-  get '/home', to: 'videos#index'
-  get '/home/:id', to: 'videos#show', as: 'video'
+  root to: 'videos#index'
   get '/genre/:name', to: 'categories#show', as: 'category'
+
+  resources :videos, only: [:show] do
+    collection do
+      get '/search', to: 'videos#search'
+    end
+  end
 end
