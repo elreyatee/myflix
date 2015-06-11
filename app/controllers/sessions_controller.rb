b64b4c3ebdc7f45a849f38737cd6b379761a1ada
Ellery Temple Jr <elreyatee@gmail.com>
2015-06-10 22:10:30 -0700

    cleaned up use of quotation marks and changed back to use of find_by as originally used

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.where(email: params[:email]).first
+    user = User.find_by(email: params[:email])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
