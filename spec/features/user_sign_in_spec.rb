5a1da9075c4f64826f82f0b08505f00eecdb80a1
Ellery Temple Jr <elreyatee@gmail.com>
2015-06-25 00:30:03 -0700

    cleaned up sign_in feature spec

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -1,26 +1,20 @@
 require 'spec_helper'
 
 feature "the sign in process" do  
-  background do 
-    User.create(name: "Aaron", email: "aaron@temple.com", password: "password")
-  end
+  given(:aaron) { Fabricate(:user, email: "aaron@temple.com", password: "password") }
 
   scenario "signing in with correct credentials" do  
-    visit '/sign_in'
-    within "fieldset#sign_in" do  
-      fill_in "Email", with: "aaron@temple.com"
-      fill_in "Password", with: "password"
-    end
+    visit sign_in_path
+    fill_in "Email", with: aaron.email
+    fill_in "Password", with: aaron.password
     click_button "Sign in"
     expect(page).to have_content("You are signed in.")
   end
 
   scenario "signing in with incorrect credentials" do  
-    visit '/sign_in'
-    within "fieldset#sign_in" do  
-      fill_in "Email", with: "aaron@temple.com"
-      fill_in "Password", with: "wrong"
-    end
+    visit sign_in_path
+    fill_in "Email", with: aaron.email
+    fill_in "Password", with: "wrong"
     click_button "Sign in"
     expect(page).to have_content("Invalid email or password.")
   end 
