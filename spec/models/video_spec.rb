92cb8c2f34a495a54f14b1f0c76b7c6e183dc976
Ellery Temple Jr <elreyatee@gmail.com>
2015-06-30 20:38:48 -0700

    used memoization technique in spec macro

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,19 +6,19 @@ describe Video do
   it { should validate_presence_of(:description) }
   it { should have_many(:reviews).order("created_at DESC") }
 
-  describe "search_by_title" do
+  describe "#search_by_title" do
 
     it "returns an empty array if there is not match" do
       batman = Video.create(title: "Batman", description: "Comic book movie!")
       superman = Video.create(title: "Superman", description: "Super hero movie!")
       expect(Video.search_by_title("oops")).to eq([])
     end
-    
+
     it "returns an array of one video for an exact match" do
       batman = Video.create(title: "Batman", description: "Comic book movie!")
       expect(Video.search_by_title("Batman")).to eq([batman])
     end
-    
+
     it "returns an array of one video for a partial match" do
       batman = Video.create(title: "Batman", description: "Comic book movie!")
       expect(Video.search_by_title("bat")).to eq([batman])
--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -1,5 +1,5 @@
-def sign_in(some_user=nil)
-  user = some_user || Fabricate(:user)
+def sign_in(user = nil)
+  user ||= Fabricate(:user)
   visit sign_in_path
   fill_in "Email", with: user.email
   fill_in "Password", with: user.password
