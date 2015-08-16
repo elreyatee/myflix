ae9f590e21be17fa01815e334f151094e06c051d
Ellery Temple Jr <elreyatee@gmail.com>
2015-08-16 15:57:18 -0700

    added seed data for relationships

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -4,6 +4,10 @@ describe Relationship do
   it { should belong_to(:user) }
   it { should belong_to(:following) }
   it do 
+    should validates(:cannot_follow_self).
+    with_message('You cannot follow yourself')
+  end
+  it do 
     should validate_uniqueness_of(:following_id).
     scoped_to(:user_id).
     with_message('You are already following this user')
