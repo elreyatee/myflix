bc8e22026ee64f4271b10ed7cc7c6c9413157352
Ellery Temple Jr <elreyatee@gmail.com>
2015-08-30 14:55:25 -0700

    changed default_url_options for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,7 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { host: 'https://tealeaf-myflixapp.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   
