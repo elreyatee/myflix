327b19e86b2963d7d4f9c534135a754cc1c11677
Ellery Temple Jr <elreyatee@gmail.com>
2015-08-30 15:02:37 -0700

    fixed domain settings for mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,27 +4,20 @@ Myflix::Application.configure do
   config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
   config.serve_static_assets = false
-
   config.assets.compress = true
   config.assets.js_compressor = :uglifier
-
   config.assets.compile = false
-
   config.assets.digest = true
-
   config.i18n.fallbacks = true
-
   config.active_support.deprecation = :notify
   config.action_mailer.default_url_options = { host: 'https://tealeaf-myflixapp.herokuapp.com' }
-
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
     address:              ENV['MAILGUN_SMTP_SERVER'],
     port:                 ENV['MAILGUN_SMTP_PORT'],
-    domain:               'tealeaf-myflixapp.herokuapp.com',
+    domain:               'https://tealeaf-myflixapp.herokuapp.com',
     user_name:            ENV['MAILGUN_SMTP_LOGIN'],
     password:             ENV['MAILGUN_SMTP_PASSWORD'],
     authentication:       'plain',
