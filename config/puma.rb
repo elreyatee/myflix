abbee612e30baa564529aeb99c655a2a00d88b1c
Ellery Temple Jr <elreyatee@gmail.com>
2015-09-08 01:41:50 -0700

    added sidekiq hack to puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -11,5 +11,6 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  @sidekiq_pid ||= spawn("bundle exec sidekiq")
   ActiveRecord::Base.establish_connection
 end
