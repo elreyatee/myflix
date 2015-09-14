f43bd4ca7338449732eca2e7eceffbf113309a67
Ellery Temple Jr <elreyatee@gmail.com>
2015-09-14 14:32:57 -0700

    added Admin::VideosController spec for GET new

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -1,13 +1,17 @@
-class Admin::VideosController < AdminController
+class Admin::VideosController < ApplicationController
+  before_action :require_user
+  before_action :require_admin
+
   def new 
-    @category_options = Category.all.map { |c| [c.name, c.id] }
     @video = Video.new
+    @category_options = Category.all.map { |c| [c.name, c.id] }
   end
 
   def create
-    @video = Video.new(video_params)
+    video = Video.new(video_params)
+    video.category = Category.find(params[:video][:category_id])
 
-    if @video.save
+    if video.save
       flash[:notice] = "Your video has been added"
       redirect_to 'back'
     else
@@ -19,6 +23,13 @@ class Admin::VideosController < AdminController
   private
 
   def video_params
-    params.require(:video).permit(:title, :category, :description, :large_cover_url, :small_cover_url)
+    params.require(:video).permit(:title, :description, :large_cover_url, :small_cover_url)
+  end
+
+  def require_admin
+    unless current_user.admin?
+      redirect_to home_path 
+      flash[:error] = "You are not authorized to do that"
+    end
   end
 end
--- app/controllers/admin_controller.rb
+++
@@ -1,8 +0,0 @@
-class AdminController < ApplicationController
-  before_action :require_user
-  before_action :require_admin
-
-  def require_admin
-    redirect_to sign_in_path unless current_user.admin? 
-  end
-end
